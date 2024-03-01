45366adf411764ee968506ce92052607f9714299
Harjeet Singh <113464174+coderHARJEET@users.noreply.github.com>
2024-03-01 11:42:17 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@2Dmatrix-II/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int rowIndex=0;
+        int colIndex=col-1;
+
+        while(rowIndex<row && colIndex>=0){
+            int element=matrix[rowIndex][colIndex];
+
+            if(element==target){
+                return 1;
+            }
+            if(element<target){
+                rowIndex++;
+            }
+            else{
+                colIndex--;
+            }
+        }
+        return 0;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1,   4,  7, 11, 15},
+        {2,   5,  8, 12, 19},
+        {3,   6,  9, 16, 22},
+        {10, 13, 14, 17, 24},
+        {18, 21, 23, 26, 30}
+    };
+    int target = 5;
+    cout << "Matrix contains target: " << (sol.searchMatrix(matrix, target) ? "true" : "false") << endl;
+    return 0;
+}
